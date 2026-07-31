848e41755fb4eba4df9e4eecbeab7e97dc27da32
Himanshu <himanshu.23bai10041@vitbhopal.ac.in>
2026-07-31 14:01:57 +0530

    Implement second solution for minimum pushes calculation
    Added a second solution for calculating minimum pushes to type a word, utilizing frequency mapping and sorting.

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -18,3 +18,31 @@ public:
         return ans;
     }
 };
+
+
+
+// Solutoion 2 can be 
+
+
+
+class Solution {
+public:
+    int minimumPushes(string s) {
+        map<char, int> freq;
+        for(char i :s)
+        {
+            freq[i]++;
+        }
+        vector<pair<char, int>> v(freq.begin(), freq.end());
+        sort(v.begin(), v.end(), [](pair<char, int> a , pair<char, int> b){
+            return a.second>b.second;
+        });
+
+        int ans = 0;
+        for(int i = 0; i<v.size(); i++)
+        {
+            ans= ans+ (v[i].second)*(i/8+1);
+        }
+        return ans;
+    }
+};
