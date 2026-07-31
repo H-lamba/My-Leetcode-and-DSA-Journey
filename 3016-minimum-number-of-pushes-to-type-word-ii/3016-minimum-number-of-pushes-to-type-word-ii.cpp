38a652b4e55b45f65329dcc553e975ab7d2875b9
Himanshu <himanshu.23bai10041@vitbhopal.ac.in>
2026-07-31 14:00:29 +0530

    Time: 5 ms (79.89%), Space: 25.5 MB (72.47%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minimumPushes(string s) {
-        map<char, int> freq;
-        for(char i :s)
-        {
-            freq[i]++;
+        int freq[26] = {0};
+        
+        for(char c : s) {
+            freq[c - 'a']++;
         }
-        vector<pair<char, int>> v(freq.begin(), freq.end());
-        sort(v.begin(), v.end(), [](pair<char, int> a , pair<char, int> b){
-            return a.second>b.second;
-        });
-
+        sort(freq, freq + 26, greater<int>());
+        
         int ans = 0;
-        for(int i = 0; i<v.size(); i++)
-        {
-            ans= ans+ (v[i].second)*(i/8+1);
+        for(int i = 0; i < 26; i++) {
+            if (freq[i] == 0) break;
+            
+            ans += freq[i] * (i / 8 + 1);
         }
+        
         return ans;
     }
 };
