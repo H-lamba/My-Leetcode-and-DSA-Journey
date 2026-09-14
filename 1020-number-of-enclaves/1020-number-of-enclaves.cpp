cf257674b9dc04055d90f0e70b0675487113a74e
Himanshu <himanshu.23bai10041@vitbhopal.ac.in>
2026-09-14 22:17:10 +0530

    Time: 25 ms (10.96%), Space: 34.7 MB (44.68%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        queue<vector<int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i = 0; i<m; i++)
+        {
+            if(grid[0][i] == 1)
+            {
+                q.push({0,i});
+                grid[0][i] = 0;
+            }
+            if(grid[n-1][i] == 1)
+            {
+                q.push({n-1,i});
+                grid[n-1][i] = 0;
+            }
+        }
+        for(int i = 0; i<n; i++)
+        {
+            if(grid[i][0] == 1)
+            {
+                q.push({i,0});
+                grid[i][0] = 0;
+            }
+            if(grid[i][m-1] == 1)
+            {
+                q.push({i,m-1});
+                grid[i][m-1] = 0;
+            }
+        }
+        vector<int> dx = {-1,1,0,0};
+        vector<int> dy = {0,0,-1,1};
+        while(!q.empty())
+        {
+            int x = q.front()[0];
+            int y = q.front()[1];
+            q.pop();
+            for(int i = 0; i<4; i++)
+            {
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny] == 1)
+                {
+                    q.push({nx, ny});
+                    grid[nx][ny] = 0;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                ans+= grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
